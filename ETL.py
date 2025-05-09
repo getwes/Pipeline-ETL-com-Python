94cbcb2ff3906e58d6f9dd8230c2b8c7baa8f0cc
unknown <wesleyfreire1707@gmail.com>
2025-05-09 16:31:34 -0300

    atualização

--- ETL.py
+++ ETL.py
@@ -2,6 +2,8 @@ import requests
 #import pandas as pd
 import json
 
+#ETL - extract, Trasform, Load
+
 #funcão para extrair dados da API DummyJSON
 def extract_data(endpoint):
     response = requests.get(endpoint)
@@ -11,11 +13,15 @@ def extract_data(endpoint):
         print(f"erro ao extrairi dados da API: {response.sttus_code}")
         return None
 
-
-
-endpoint_users = "https://dummyjson.com/users/1"
+def load_data(data,path):
+    id = data["id"]
+    #dentro da pasta path, criar um arquivo json com o nome do arquivo sendo o id o nome do arquivo .json
+    with open(f"{path}/{id}.json", "w") as file:
+        json.dump(data, file)
 endpoint_products = "https://dummyjson.com/products/1"
 
-data_users = extract_data(endpoint_users)
+endpoint_users = "https://dummyjson.com/users/"
 
-print(data_users)
+for i in range(1,  10):
+    data_users = extract_data(endpoint_users + str(i))
+    load_data(data_users, "users")
